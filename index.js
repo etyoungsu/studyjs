d4d0e9c1cef571f1f47ab6581b8ba7ad831b2ebf
etyoungsu <etyoungsu@ajou.ac.kr>
2021-02-02 23:38:12 +0900

    Practice Ends

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //alert('Im Working. Im JS');
 //console.log('Im Working. Im JS again');
-
+/*
 const calculator = {
     plus: function (a, b) {
         return a + b;
@@ -30,3 +30,41 @@ function handleClick() {
 }
 
 title.addEventListener("click", handleClick);
+
+
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
+
+function init() {
+    title.style.color = BASE_COLOR;
+}
+
+function handleClick() {
+    if (title.style.color === BASE_COLOR)
+        title.style.color = OTHER_COLOR;
+    else
+        title.style.color = BASE_COLOR;
+}
+
+init();
+title.addEventListener("click", handleClick);
+// mouthenter 도 가능
+
+*/
+const title = document.querySelector("#title");
+
+const CLICKED_CLASS = "clicked";
+
+function handleClick() {
+    const hasClass = title.classList.contains(CLICKED_CLASS);
+    if (!hasClass)
+        title.classList.add(CLICKED_CLASS);
+    else
+        title.classList.remove(CLICKED_CLASS);
+}
+
+function init() {
+    title.addEventListener("click", handleClick);
+}
+
+init();
